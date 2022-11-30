70bc2c0f84eb67550ce61dafaf0cd0080814dc06
NaorFiss <naorpa29@gmail.com>
2022-11-30 14:46:54 +0200

    homepage

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,20 +2,20 @@
   <section class="main-container">
     <div class="main-content">
       <div class="main-layout">
-        <div class="hero-wrapper">
-          <div class="hero-title">
+        <div class="hero-wrapper flex column">
+          <div class="hero-title ">
             <p>
               Find the perfect freelance services for your business
             </p>
             <div class="search-bar">
-              <form>
+              <form class="flex">
                 <input type="search" placeholder="Try building mobile app" value>
-                <button class=" submit">search
+                <button class="submit" @click="goToExplore">search
                 </button>
               </form>
             </div>
-            <div class="popular-gigs">" Popular"
-              <ul>
+            <div class="popular-gigs flex">" Popular"
+              <ul class="flex">
                 <li>Website Design</li>
                 <li>WordPress</li>
                 <li>Logo Design</li>
@@ -47,6 +47,9 @@ export default {
   created() {
   },
   methods: {
+    goToExplore() {
+      this.$router.push('/gig')
+    }
   }
 
 }
