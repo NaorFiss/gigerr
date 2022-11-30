75e3e66ec927634f20c6a5e79cf081ef3f4de558
NaorFiss <naorpa29@gmail.com>
2022-11-30 13:44:05 +0200

    home page design

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,36 @@
 <template>
-  <section class="container home text-center">
-    <h1>Home Page</h1>
-    <h5 class="text-center">
-      Powered up Frontend
-    </h5>
-    <img alt="Vue logo" src="../assets/logo.png">
+  <section class="main-container">
+    <div class="main-content">
+      <div class="main-layout">
+        <div class="hero-wrapper">
+          <div class="hero-title">
+            <p>
+              Find the perfect freelance services for your business
+            </p>
+            <div class="search-bar">
+              <form>
+                <input type="search" placeholder="Try building mobile app" value>
+                <button class=" submit">search
+                </button>
+              </form>
+            </div>
+            <div class="popular-gigs">" Popular"
+              <ul>
+                <li>Website Design</li>
+                <li>WordPress</li>
+                <li>Logo Design</li>
+                <li>Video Editing</li>
+              </ul>
+            </div>
+          </div>
+          <div class="hero backgrounds">
+
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
+
 
 
 </template>
