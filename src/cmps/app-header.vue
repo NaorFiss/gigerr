bce1b85f0d5353ed47a8b19615320b929f48a172
itzhake <itzikil@icloud.com>
2022-12-01 17:54:34 +0200

    2.3

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <header >
     <div class="hamburger">
       <svg xmlns="http://www.w3.org/2000/svg" width="23" height="19" viewBox="0 0 23 19">
         <rect y="16" width="23" height="3" rx="1.5" fill="#555"></rect>
