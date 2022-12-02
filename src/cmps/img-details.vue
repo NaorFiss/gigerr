024079ae1ee4519c04f0e662f83933792a383a4b
NaorFiss <naorpa29@gmail.com>
2022-12-02 10:40:13 +0200

    vc

---
+++ src/cmps/img-details.vue
@@ -0,0 +1,18 @@
+<template>
+    <el-carousel height="400px" :interval="0" trigger="click">
+        <el-carousel-item class="main-img" v-for="item in imgs" :key="item">
+            <img :src="item" alt="">
+        </el-carousel-item>
+    </el-carousel>
+</template>
+
+<script>
+export default {
+    name: 'gig-list',
+    props: {
+        imgs: Array
+    },
+    components: {
+    }
+}
+</script>
