9de8573ef4c0480331824cefa8e83f31d6f53888
NaorFiss <naorpa29@gmail.com>
2022-12-05 00:25:44 +0200

    mor about review

--- src/cmps/details-review.vue
+++ src/cmps/details-review.vue
@@ -1,37 +1,38 @@
 <template>
-    <div class="details-review flex" v-if=gig>
-        <div class="user-img-container"></div>
-        <section class="main-details-review">
+    <div class="details-review flex " v-if=gig>
+        <div class="user-img-container">img-container</div>
+        <section class="main-details-review flex column">
             <div class="user-review-name">
                 {{ gig.likedByUsers[0].username }}
             </div>
-            <div class="user-review-country">
-                <div class="country-flag"></div>
-                <p class="country-name"></p>
+            <div class="user-review-country flex">
+                <div class="country-flag">flag </div> |
+                <p class="country-name">country name</p>
             </div>
-            <div class="performance-grade">
-                <div class="performance-stars"></div>
-                <div class="performance-date"></div>
-                <div class="review-txt">
-                    I am satisfied with my first experience on fiverr and with mohsinfancy. Communication was good, even
-                    though
-                    sometimes a
-                    bit short and informal. I am happy with the solid design skill, even if nothing extraordinary. You
-                    can tell
-                    he
-                    has his
-                    style or template but this is ok, as it fits well with what I was looking for. All in all very good
-                    price/performance
-                    ratio
-                </div>
-                <section class="helpful-container">
-                    <span class="helpful">helpful?</span>
-                    <div class="like"></div>
-                    <div class="yes">Yes</div>
-                    <div class="deslike"></div>
-                    <div class="no">No</div>
-                </section>
+            <div class="performance-grade flex">
+                <div class="performance-stars">stars </div> |
+                <div class="performance-date">date</div>
             </div>
+            <div class="review-txt">
+                I am satisfied with my first experience on fiverr and with mohsinfancy. Communication was good, even
+                though
+                sometimes a
+                bit short and informal. I am happy with the solid design skill, even if nothing extraordinary. You
+                can tell
+                he
+                has his
+                style or template but this is ok, as it fits well with what I was looking for. All in all very good
+                price/performance
+                ratio
+            </div>
+            <section class="helpful-container flex">
+                <span class="helpful">helpful? </span>
+                <div class="like">like </div>
+                <div class="yes">Yes </div>
+                <div class="deslike">deslike </div>
+                <div class="no">No</div>
+            </section>
+
         </section>
     </div>
 </template>
